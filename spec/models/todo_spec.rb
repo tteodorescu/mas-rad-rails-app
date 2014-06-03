7729c9349528a58fde6eea378dee1343004b03b2
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2014-06-03 15:26:11 +0200

    Add todo scopes specs

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -5,4 +5,21 @@ describe Todo do
     todo = Todo.new(title: '')
     expect(todo.valid?).to eq(false)
   end
+
+  describe 'scopes' do
+    let!(:completed_todo) { Todo.create(title: 'done', completed: true) }
+    let!(:active_todo) { Todo.create(title: 'to do', completed: false) }
+
+    describe '.completed' do
+      it 'returns just completed todos' do
+        expect(Todo.completed).to eq [completed_todo]
+      end
+    end
+
+    describe '.active' do
+      it 'returns just active todos' do
+        expect(Todo.active).to eq [active_todo]
+      end
+    end
+  end
 end
